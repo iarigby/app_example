557ee4ae573fb43d4243bf541c52619d499d8299
ia rigby <marvinzem@gmail.com>
2022-05-24 12:03:16 +0400

    ხმების დათვლის იმპლემენტაცია

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ function castBallot() {
     }
     ballots.push(ballot)
 }
+const finalVotes = {
+    "party-1": 0,
+    "party-2": 0,
+    "party-3": 0
+}
+
+function countVotes() {
+    for (let i = 0; i < ballots.length; i++) {
+        const ballot = ballots[i]
+        const choice = ballot.party
+        finalVotes[choice]++
+    }
+    console.log(finalVotes)
+}
